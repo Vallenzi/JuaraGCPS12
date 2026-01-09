da14fc4ab2a5de2f4e26b87609cddbf94af28003
Vallenzi <60637845+Vallenzi@users.noreply.github.com>
2026-01-10 00:03:40 +0700

    Create valllenz.sh

---
+++ Automate Data Capture at Scale with Document AI: Challenge Lab/valllenz.sh
@@ -0,0 +1,148 @@
+# Set text styles
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+clear 
+
+echo "MASUKAN DATA BERIKUT SESUAI DENGAN LAB"
+read -p "${BLUE_TEXT}${BOLD_TEXT}Masukan REGION: ${RESET}" REGION
+read -p "${BLUE_TEXT}${BOLD_TEXT}Masukan ID PROCESSOR: ${RESET}" PROCESSOR
+
+# Export variables after collecting input
+export REGION PROCESSOR
+
+
+gcloud auth list
+
+export PROJECT_ID=$(gcloud config get-value core/project)
+
+gcloud services enable documentai.googleapis.com --project $DEVSHELL_PROJECT_ID
+
+sleep 10
+
+
+mkdir ./document-ai-challenge
+gsutil -m cp -r gs://spls/gsp367/* \
+  ~/document-ai-challenge/
+
+
+ACCESS_CP=$(gcloud auth application-default print-access-token)
+
+curl -X POST \
+  -H "Authorization: Bearer $ACCESS_CP" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "display_name": "'"$PROCESSOR"'",
+    "type": "FORM_PARSER_PROCESSOR"
+  }' \
+  "https://documentai.googleapis.com/v1/projects/$PROJECT_ID/locations/us/processors"
+
+gsutil mb -c standard -l $REGION -b on gs://$PROJECT_ID-input-invoices
+gsutil mb -c standard -l $REGION -b on gs://$PROJECT_ID-output-invoices
+gsutil mb -c standard -l $REGION -b on gs://$PROJECT_ID-archived-invoices
+
+bq --location=US mk  -d \
+ --description "Form Parser Results" \
+ ${PROJECT_ID}:invoice_parser_results
+ 
+cd ~/document-ai-challenge/scripts/table-schema
+
+bq mk --table \
+invoice_parser_results.doc_ai_extracted_entities \
+doc_ai_extracted_entities.json
+
+cd ~/document-ai-challenge/scripts
+
+PROJECT_ID=$(gcloud config get-value project)
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$PROJECT_ID" --format='value(project_number)')
+
+SERVICE_ACCOUNT=$(gcloud storage service-agent --project=$PROJECT_ID)
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member serviceAccount:$SERVICE_ACCOUNT \
+  --role roles/pubsub.publisher
+
+
+export CLOUD_FUNCTION_LOCATION=$REGION
+
+echo $CLOUD_FUNCTION_LOCATION
+
+sleep 30
+
+#!/bin/bash
+
+deploy_function() {
+  gcloud functions deploy process-invoices \
+  --gen2 \
+  --region=${CLOUD_FUNCTION_LOCATION} \
+  --entry-point=process_invoice \
+  --runtime=python313 \
+  --service-account=${PROJECT_ID}@appspot.gserviceaccount.com \
+  --source=cloud-functions/process-invoices \
+  --timeout=400 \
+  --env-vars-file=cloud-functions/process-invoices/.env.yaml \
+  --trigger-resource=gs://${PROJECT_ID}-input-invoices \
+  --trigger-event=google.storage.object.finalize \
+  --service-account $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+  --allow-unauthenticated
+}
+
+deploy_success=false
+
+while [ "$deploy_success" = false ]; do
+  if deploy_function; then
+    echo "Function deployed successfully.. Welcome to VALLENZI TECH"
+  deploy_success=true
+  else
+    echo "Deployment Retrying"
+    sleep 20
+  fi
+done
+
+PROCESSOR_ID=$(curl -X GET \
+  -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+  -H "Content-Type: application/json" \
+  "https://documentai.googleapis.com/v1/projects/$PROJECT_ID/locations/us/processors" | \
+  grep '"name":' | \
+  sed -E 's/.*"name": "projects\/[0-9]+\/locations\/us\/processors\/([^"]+)".*/\1/')
+
+export PROCESSOR_ID
+echo $PROCESSOR_ID
+
+export CLOUD_FUNCTION_LOCATION=$REGION
+echo $CLOUD_FUNCTION_LOCATION
+
+export PROJECT_ID=$(gcloud config get-value core/project)
+echo $PROJECT_ID
+
+gcloud functions deploy process-invoices \
+  --gen2 \
+  --region=${CLOUD_FUNCTION_LOCATION} \
+  --entry-point=process_invoice \
+  --runtime=python39 \
+  --service-account=${PROJECT_ID}@appspot.gserviceaccount.com \
+  --source=cloud-functions/process-invoices \
+  --timeout=400 \
+  --trigger-resource=gs://${PROJECT_ID}-input-invoices \
+  --trigger-event=google.storage.object.finalize \
+  --update-env-vars=PROCESSOR_ID=${PROCESSOR_ID},PARSER_LOCATION=us,PROJECT_ID=${PROJECT_ID} \
+  --service-account=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
+
+
+export PROJECT_ID=$(gcloud config get-value core/project)
+gsutil -m cp -r gs://cloud-training/gsp367/* \
+~/document-ai-challenge/invoices gs://${PROJECT_ID}-input-invoices/
+
+# COMPLETE
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}SILAHKAN CEKLIS CHECKPOIN${RESET_FORMAT}"
+echo
